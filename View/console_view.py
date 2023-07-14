6e24e43815d6e44db8806f14e5dac98286d9edb0
ZandrexQX <91482671+ZandrexQX@users.noreply.github.com>
2023-07-14 07:48:47 +0300

    Add color

--- View/console_view.py
+++ View/console_view.py
@@ -1,12 +1,12 @@
 from NotesApp.Presenter.App import *
-
-print("Note_console")
+from colorama import Fore, Back, Style
+print(Fore.BLUE + "Note_console")
 
 flag = True
 app = App()
 
 while flag:
-    print("Введите команду:\n"
+    print(Fore.YELLOW + "Введите команду:\n"
           "1. Создать заметку\n"
           "2. Редактировать заметку\n"
           "3. Удалить заметку\n"
@@ -14,44 +14,44 @@ while flag:
           "5. Сохранить заметки\n"
           "6. Вывести все заметки\n"
           "7. Выход")
-    in_data = input("Ввод: ")
+    in_data = input(Fore.BLUE + "Ввод: ")
     match in_data:
         case "1":
-            title = input("Введите название: ")
-            description = input("Введите описание: ")
+            title = input(Fore.BLUE + "Введите название: ")
+            description = input(Fore.BLUE + "Введите описание: ")
             app.create_note(title, description)
             app.add_list()
-            print(f"Заметка {title} добавлена")
+            print(Fore.GREEN + f"Заметка {title} добавлена")
         case "2":
-            title = input("Введите название редактируемой заметки: ")
+            title = input(Fore.BLUE + "Введите название редактируемой заметки: ")
             if app.note_list.check_note(title):
                 note = app.note_list.get_note(title)
-                descr = input(f"Заметка {title} найдена. Введите новое описание: ")
+                descr = input(Fore.YELLOW + f"Заметка {title} найдена. Введите новое описание: ")
                 note.set_description(descr)
-                print("Заметка изменена")
+                print(Fore.GREEN + "Заметка изменена")
         case "3":
-            note = input("Введите название удаляемой заметки: ")
+            note = input(Fore.BLUE + "Введите название удаляемой заметки: ")
             if app.note_list.check_note(title):
                 app.del_note(title)
-                print(f"Заметка {title} удалена")
+                print(Fore.GREEN + f"Заметка {title} удалена")
             else:
-                print("Такой заметки нет")
+                print(Fore.RED + "Такой заметки нет")
         case "4":
             app.read_from_csv()
-            print("Заметки загружены")
+            print(Fore.GREEN + "Заметки загружены")
         case "5":
             app.save_notes()
-            print("Заметки сохранены")
+            print(Fore.GREEN + "Заметки сохранены")
         case "6":
             list = app.note_list.get_list()
             if len(list) != 0:
-                print("Список заметок:")
+                print(Fore.GREEN + "Список заметок:")
                 for note in list:
                     print(note.get_note())
-            else: print("Cписок заметок пустой")
+            else: print(Fore.RED + "Cписок заметок пустой")
 
         case "7":
             flag = False
-            print("Exit Note_console")
+            print(Fore.BLUE + "Exit Note_console")
         case _:
             pass
